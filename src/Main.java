c9b6f52887eb19d7051881242025f7505c3fcf58
Selcuk Tekin <selcuk.tekin.nl@gmail.com>
2017-11-29 23:23:43 +0100

    shieeet

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,59 @@
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Main {
 
-    static String SERVER_ADDRESS = "localhost";
-    static int SERVER_PORT = 1337;
-
-    static Socket socket;
+    private static Socket socket;
+    private static OutputStream outputStream;
+    private static InputStream inputStream;
 
     public static void main(String[] args) {
+        final String SERVER_ADDRESS = "localhost";
+        final int SERVER_PORT = 1337;
+
         try {
-            // Maak verbinding met de server
             socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+            inputStream = socket.getInputStream();
+            outputStream = socket.getOutputStream();
 
-            // Haal de input- en output stream uit de socket op
-            InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream = socket.getOutputStream();
+            new Thread(new InputHandler()).start();
+            new Thread(new OutputHandler()).start();
 
-            // Verstuur een regel tekst
+        } catch (IOException e) {
+            System.out.println("Probleem bij opstarten");
+        }
+    }
+
+    public static class OutputHandler implements Runnable {
+
+        @Override
+        public void run() {
+            Scanner scanner = new Scanner(System.in);
             PrintWriter writer = new PrintWriter(outputStream);
-            writer.println("hallo");
-            writer.flush(); // Vertel het besturingssysteem om alle uitstaande data te versturen
 
             while(true) {
-                // Blokkeer de thread tot er een volledige regel binnenkomt
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                String line = reader.readLine();
-                System.out.println(line);
+                String command = scanner.nextLine();
+                writer.println(command);
+                writer.flush();
             }
-        } catch (IOException e) {
+        }
+    }
 
+    public static class InputHandler implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                while(true) {
+                    String line = reader.readLine();
+                    System.out.println(line);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
